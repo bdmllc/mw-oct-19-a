4fceaf499915ca519f8124b7936da88e71cbe763
bdmllc <beachdaymedia@gmail.com>
2019-10-29 17:06:22 -0400

    changed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import styles from "../css/navbar.module.css"
 import { FaAlignRight } from "react-icons/fa"
 import links from "../constants/links"
 import socialIcons from "../constants/social-icons"
-import logo from "../images/MW-logo-Oct-19.svg"
+import logo from "../images/mw-logo-oct-29-19-c.svg"
 
 const Navbar = () => {
   const [isOpen, setNav] = useState(false)
