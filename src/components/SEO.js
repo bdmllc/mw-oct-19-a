d47bc0cd3726cfb7a665fd656452ad72bd605bd8
bdmllc <beachdaymedia@gmail.com>
2019-10-29 05:51:28 -0400

    copied Johns code for seo.js and config file

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,15 +1,12 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-
 const getData = graphql`
-  query {
+  {
     site {
       siteMetadata {
         siteTitle: title
         siteDesc: description
-        author
-        siteUrl
         author
         siteUrl
         image
@@ -18,7 +15,6 @@ const getData = graphql`
     }
   }
 `
-
 const SEO = ({ title, description }) => {
   const { site } = useStaticQuery(getData)
   const {
@@ -33,12 +29,21 @@ const SEO = ({ title, description }) => {
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
-      {/*twitter card*/}
+      {/* open graph  */}
+      {/* “Open Graph” meta tags */}
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
+      {/* twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`${image} ${siteUrl}`} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
