2da39af8dba040805fa0ccf97b731316b3c50861
bdmllc <beachdaymedia@gmail.com>
2019-10-28 21:19:06 -0400

    changed image in static folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: "A Miami Blog In Search Of New Experiences.",
     author: "Reinhold Lehmbruck",
     twitterUsername: "@miami_walking",
-    image: "/Manati_Cropped.jpg",
+    image: "Manati_Cropped.jpeg",
     siteUrl: "https://miamiwalking.netlify.com",
   },
   plugins: [
